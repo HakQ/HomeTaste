6bc80065a964628abc9987b66ec8359533bb8ba1
stanmill <stanley.pena29@gmail.com>
2018-11-09 18:51:49 -0500

    Update home-taste/src/components/Home.js

--- home-taste/src/components/Home.js
+++ home-taste/src/components/Home.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import BackgroundImage from 'react-background-image-loader';
 
-
 export default class Home extends React.Component {
+
+    foodSearch(event){
+
+        let url = "deac5308423f7cc3a692e3a8c9d108c6" + event.target.value;
+    
+        fetch(url).
+        then((response) => {
+            console.log(Response.status);
+            
+            if(Response.status == 200){
+                return response.json;
+            }else{
+                throw "Not Found";
+            }
+        })
+        .then((jsonData) => {
+            console.log(jsonData);
+            this.setState({data: jsonData});
+        })
+        .catch((error) =>{
+            console.log(error);
+        });
+    }
     render() {
         return (
             <div> 
