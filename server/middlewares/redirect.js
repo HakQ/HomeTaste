d159b907cb2f06bbd7b8ee9c5eef74e236ec4583
Qiuqun Wang <qiuqunwang12@gmail.com>
2018-11-26 18:48:11 -0500

    test

--- server/middlewares/redirect.js
+++ server/middlewares/redirect.js
@@ -1,7 +1,6 @@
 const redirect = {};
 
 redirect.ifLoggedIn = (route) =>
-<<<<<<< HEAD
   (req, res, next) => (req.user ? res.redirect(route) : next());
 
 redirect.ifNotLoggedIn = (route = '/login') =>
@@ -9,11 +8,5 @@ redirect.ifNotLoggedIn = (route = '/login') =>
 
 redirect.ifNotAuthorized = (route) =>
   (req, res, next) => (req.user.username !== req.params.username ? res.redirect(route) : next());
-=======
-	(req, res, next) => (req.user ? res.redirect(route) : next());
-
-redirect.ifNotLoggedIn = (route = '/login') =>
-	(req, res, next) => (req.user ? next() : res.redirect(route));
->>>>>>> 52225259e60746a1fa75e7892b509cac6f7b637b
 
 module.exports = redirect;
