38aac34eca0595a27b3dba067b84d51f0c3d43d0
stanmill <stanley.pena29@gmail.com>
2018-12-08 19:27:46 -0500

    api works fine
    now i just ogtta perform optimization

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-
+import PropTypes from 'prop-types';
 
 class Recipe extends React.Component {
-
-    state = {
-      activeRecipe: []
-    }
-    
     render() {
-      const recipe = this.state.activeRecipe;
-      console.log("inside recipe");
+      const {recipe} = this.props;
       return (
-        
         <div className="container">
-          { this.state.activeRecipe.length !== 0 &&
+          
             <div className="active-recipe">
               <img className="active-recipe__img" src={recipe.image_url} alt={recipe.title}/>
               <h3 className="active-recipe__title">{ recipe.title }</h3>
@@ -22,16 +15,18 @@ class Recipe extends React.Component {
                 Publisher: <span>{ recipe.publisher }</span>
               </h4>
               <p className="active-recipe__website">Website: 
-                <span><a href={recipe.publisher_url}>{recipe.publisher_url}</a></span>
+                <span><a href={recipe.publisher_url}>{recipe.f2f_url}</a></span>
               </p>
               <button className="active-recipe__button">
                 <Link to="/">Go Home</Link>
               </button>
             </div>
-          }
         </div>
       );
-    }
-  };
+  }
+}
 
+  Recipe.propTypes = {
+   recipe : PropTypes.object.isRequired
+  }
   export default Recipe;
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,37 +1,80 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
+import Recipe from './Recipe';
+
+/*let recipes = [
+  {publisher: "Epicurious", f2f_url: "http://food2fork.com/view/f22836", title: "Paletas de Aguacate", source_url: "http://www.epicurious.com/recipes/food/views/Paletas-de-Aguacate-366231", recipe_id: "f22836"},
+  {publisher: "All Recipes", f2f_url: "http://food2fork.com/view/2085", title: "Avocado-Lime Shrimp Salad (Ensalada de Camarones con Aguacate y Limon)", source_url: "http://allrecipes.com/Recipe/Avocado-Lime-Shrimp-S…ada-De-Camarones-Con-Aguacate-Y-Limon/Detail.aspx", recipe_id: "2085"},
+  {publisher: "All Recipes", f2f_url: "http://food2fork.com/view/2085", title: "Avocado-Lime Shrimp Salad (Ensalada de Camarones con Aguacate y Limon)", source_url: "http://allrecipes.com/Recipe/Avocado-Lime-Shrimp-S…ada-De-Camarones-Con-Aguacate-Y-Limon/Detail.aspx", recipe_id: "2085"}]*/  
 
 
-const Recipes = props => (
-    <div className="container">
-    <div className="row">
-    { props.recipes.map((recipe) => {
-      return (
-        <div key={recipe.title} className="col" style={{ marginBottom:"2rem" }}>
-          <div className="recipes__box" data-recipe={1}>
-            <img 
-              className="recipe__box-img" 
-              src={recipe.image_url} 
-              alt={recipe.title}/>
-              <div className="recipe__text">
-                <h5 className="recipes__title">
-                  { recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...` }
-                </h5>
-                <p className="recipes__subtitle">Publisher: <span>
-                  { recipe.publisher }
-                </span></p>
-              </div>
-              <button className="recipe_buttons">     
-              <Link to={{ 
-                  pathname: `/recipe/${recipe.recipe_id}`
-                  }}>View Recipe</Link>
-              </button>
+class Recipes extends React.Component {
+  constructor (props){
+    console.log(props);
+    super(props);
+    this.state = {
+      isRecipeInstructionShowing:true,
+      currentRecipe:null
+    }
+    this.handleRecipe = this.handleRecipe.bind(this);
+  }
+
+  handleRecipe(evt) {
+    
+    this.setState({
+      isRecipeInstructionShowing: false,
+      currentRecipeIndex: evt.target.attributes.getNamedItem('data-index').value
+    });
+
+  }
+
+  renderRecipesListing = (recipes) => (
+   
+      <div className="container">
+      <div className="row">
+      {recipes.map((recipe,indx) => (
+          <div 
+            key={recipe.title} 
+            className="col" 
+            style={{ marginBottom:"2rem" }} 
+            >
+            <div className="recipes__box">
+              <img 
+                className="recipe__box-img" 
+                src={recipe.image_url} 
+                alt={recipe.title}/>
+                <div className="recipe__text">
+                  <h5 className="recipes__title">
+                    { recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...` }
+                  </h5>
+                  <p className="recipes__subtitle">Publisher: <span>
+                    { recipe.publisher }
+                  </span></p>
+                </div>
+                <button className="recipe_buttons" onClick={this.handleRecipe} data-index={indx}>     
+                  View Recipe
+                </button>
+            </div>
           </div>
-        </div>
-      );
-    })}
+        ))
+      }
+      </div>
     </div>
-  </div>
-);
+  )
+  
+  render() {
+    const { recipes } = this.props;
+    const { currentRecipeIndex } = this.state;
+    return (
+        this.state.isRecipeInstructionShowing ? this.renderRecipesListing(recipes) : <Recipe recipe={recipes[currentRecipeIndex]}/>
+      )
+  }
+}
+
+Recipes.propTypes = {
+  recipes: PropTypes.array.isRequired,
+  currentRecipe: PropTypes.number
+}
 
 export default Recipes;
