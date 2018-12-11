076d0d24a07aac61c50eba4d5bc2c7828c0f9c0d
Qiuqun Wang <qiuqunwang12@gmail.com>
2018-12-10 19:29:25 -0500

    combined stan's work from the Home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,15 @@ import React from 'react'
 import Recipes from './Recipes';
 //import homepage from './images/homepage';
 import homepage from '../components/images/homepage.jpeg';
+import './Home.css'
+
+
+let backimg = {
+    width: "100%",
+    height: "400px",
+    backgroundImage: `url(${homepage})`
+};
+
 
 export default class Home extends React.Component {
 
@@ -12,17 +21,20 @@ export default class Home extends React.Component {
         };
     }
 
-    foodSearch(e){
-        //https://www.food2fork.com/api/search?key={API_KEY}&q=shredded%2
+   foodSearch(e){
+        if(e.target.value.length === 0){
+            window.location.reload();
+        }
 
         const recipeName = e.target.value;
         e.preventDefault();
-        const API_KEY = "d6f0630f1a2543e76bbf7b3ab2b20d8a";
+        // const API_KEY = "d6f0630f1a2543e76bbf7b3ab2b20d8a";
+        const API_KEY = "248278769b9624d2a230cd9fcf96a4d4";
+
         const API_CALL = `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`;
          fetch(API_CALL)
         .then((response) => {      
             if(response.status === 200){
-                console.log("something here "+response.json);
                 return response.json();
             }     
         })
@@ -34,14 +46,17 @@ export default class Home extends React.Component {
             console.log(error);
         });
     }
+
     render() {
         
         return (
             <div> 
-                <form>
-                    <label><h1>Home Taste Recipe Search</h1></label>   
+                <form style={backimg}>
+                    <label><h1 className={"welcomeTitle"}>Home Taste Recipe Search</h1></label>   
                 </form>  
-                <input type="text" onChange={(e) => this.foodSearch(e)}/>             
+                <div className={"col-md-8 centerMe"}>
+                    <input className={"form-control"} type="text" onChange={(e) => this.foodSearch(e)} placeholder=" Search for your food"/>
+                </div>             
                 <Recipes recipes={this.state.recipes}/>
             </div>
         )
